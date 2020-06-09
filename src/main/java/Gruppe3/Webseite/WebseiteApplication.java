6a414f2292cae93a45d4b9bea553b33adac296ae
Ludwig Roggenkamp <roggenka@fim.uni.passau.de>
2020-06-09 21:52:34 +0200

    feat: initialize spring project

---
+++ src/main/java/Gruppe3/Webseite/WebseiteApplication.java
@@ -0,0 +1,13 @@
+package Gruppe3.Webseite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebseiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebseiteApplication.class, args);
+	}
+
+}
