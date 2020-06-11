718ea4cc1bae0ec23d0fdcd7166538a4ff4e74f4
tionis.dev <git@tionis.dev>
2020-06-11 19:30:03 +0200

    feat: added multiple event support to rest example

--- src/main/java/Gruppe3/Webseite/restservice/EventsController.java
+++ src/main/java/Gruppe3/Webseite/restservice/EventsController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EventsController {
     @GetMapping("/events")
-    public Event event(@RequestParam(value = "n", defaultValue = "20") String count) {
-        return new Event("Basic", "Passau", "2020-10-01",
+    public Event[] event(@RequestParam(value = "n", defaultValue = "20") String count) {
+        // TODO instead of examples get the event array from the getLastEvents(int n) method
+        Event[] events = new Event[2];
+        events[0] = new Event("Basic", "Passau", "2020-10-01",
                 "TestEvent", "This is a test");
+        events[1] = new Event("Basic", "Passau", "2020-10-02",
+                "TestEvent Followup", "This is a second test");
+        return events;
     }
 }
