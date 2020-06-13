f5f12ff88f201b0f251ab854e32fd3e113ce6787
tionis.dev <git@tionis.dev>
2020-06-13 15:20:55 +0200

    fix: added some forgotten input parameters

---
+++ src/main/java/Gruppe3/Webseite/service/NoSuchEvent.java
@@ -0,0 +1,11 @@
+package Gruppe3.Webseite.service;
+
+public class NoSuchEvent extends Exception {
+    public NoSuchEvent(String exc) {
+        super(exc);
+    }
+
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
