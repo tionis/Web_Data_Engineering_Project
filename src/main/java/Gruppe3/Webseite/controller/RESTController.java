dacf41004f70264165556213ffed9de94569ba94
tionis.dev <git@tionis.dev>
2020-06-13 14:32:02 +0200

    fix: modified rest controller for new project structure and date data type

--- src/main/java/Gruppe3/Webseite/controller/RESTController.java
+++ src/main/java/Gruppe3/Webseite/controller/RESTController.java
@@ -1,18 +1,21 @@
-package Gruppe3.Webseite.restservice;
+package Gruppe3.Webseite.controller;
 
+import Gruppe3.Webseite.model.Event;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
-public class EventsController {
-    @GetMapping("/events")
-    public Event[] event(@RequestParam(value = "n", defaultValue = "20") String count) {
+public class RESTController {
+    @GetMapping("/api/events")
+    public Event[] events(@RequestParam(value = "n", defaultValue = "20") String count) {
         // TODO instead of examples get the event array from the getLastEvents(int n) method
         Event[] events = new Event[2];
-        events[0] = new Event("Basic", "Passau", "2020-10-01",
+        events[0] = new Event("Basic", "Passau", new Date(),
                 "TestEvent", "This is a test");
-        events[1] = new Event("Basic", "Passau", "2020-10-02",
+        events[1] = new Event("Basic", "Passau", new Date(),
                 "TestEvent Followup", "This is a second test");
         return events;
     }
