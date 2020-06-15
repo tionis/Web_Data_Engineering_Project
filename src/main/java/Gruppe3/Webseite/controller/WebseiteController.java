8240b7c7a06114902c150d5cfa9f298fa4d91dd2
Lorenz Simon <simonlorenz@fim.uni-passau.de>
2020-06-15 12:14:34 +0200

    feat: Added Container Fragment for dynamic Event List (Last Events)

--- src/main/java/Gruppe3/Webseite/controller/WebseiteController.java
+++ src/main/java/Gruppe3/Webseite/controller/WebseiteController.java
@@ -1,5 +1,6 @@
 package Gruppe3.Webseite.controller;
 
+import Gruppe3.Webseite.model.Event;
 import Gruppe3.Webseite.service.Data;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -32,5 +33,20 @@ public class WebseiteController {
         String[] types = data.getTypes();
         model.addAttribute("types", types);
         return "add_event";
+    }
+
+    /**
+     * Handles GET requests on path "/".
+     *
+     * @param model Model that transfers data between controller and view
+     * @return The home page template.
+     */
+    @GetMapping
+    public String getHome(Model model) {
+        String[] types = data.getTypes();
+        Event[] events = data.getLastEvents(20);
+        model.addAttribute("types", types);
+        model.addAttribute("events", events);
+        return "home";
     }
 }
