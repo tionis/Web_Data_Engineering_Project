dd6439eec77114805bcd021ae686a68ec84834f4
tionis.dev <git@tionis.dev>
2020-06-15 21:24:21 +0200

    fix: Marked Data Class as a (Service) Component and renamed Class feat: Added Controller Method for "add_event" Requests

--- src/main/java/Gruppe3/Webseite/controller/WebseiteController.java
+++ src/main/java/Gruppe3/Webseite/controller/WebseiteController.java
@@ -1,8 +1,36 @@
 package Gruppe3.Webseite.controller;
 
+import Gruppe3.Webseite.service.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebseiteController {
 
+    private Data data;
+
+    /**
+     * Constructs a WebseiteController
+     *
+     * @param data The data object for service methods
+     */
+    @Autowired
+    public WebseiteController(Data data) {
+        this.data = data;
+    }
+
+    /**
+     * Handles GET requests on path "/add".
+     *
+     * @param model Model that transfers data between controller and view
+     * @return The form page template.
+     */
+    @GetMapping("add")
+    public String addEvent(Model model) {
+        String[] types = data.getTypes();
+        model.addAttribute("types", types);
+        return "add_event";
+    }
 }
