774630f953bffda223d3f8869186f6b2487afc61
Tionis Nagir <tionis@tasadar.net>
2020-06-17 18:29:51 +0200

    fix: fixed create event
    Fixed create event and also input validation

--- src/main/java/Gruppe3/Webseite/controller/WebseiteController.java
+++ src/main/java/Gruppe3/Webseite/controller/WebseiteController.java
@@ -2,15 +2,20 @@ package Gruppe3.Webseite.controller;
 
 import Gruppe3.Webseite.model.Event;
 import Gruppe3.Webseite.service.Data;
+import Gruppe3.Webseite.service.NoSuchEvent;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Controller
 public class WebseiteController {
 
-    private Data data;
+    private final Data data;
 
     /**
      * Constructs a WebseiteController
@@ -53,12 +58,68 @@ public class WebseiteController {
     /**
      *
      */
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "submit.html")
-    @ResponseBody
-    public String getQuery(@RequestParam String eType, @RequestParam String eName, @RequestParam String eDesc, @RequestParam String eDate, @RequestParam String eLocation) {
-        //TODO: wenn datenbank fertig
-        return "edit here";
+    @RequestMapping(method = {RequestMethod.POST}, value = "/")
+    public String createEvent(Model model, @RequestParam String eType, @RequestParam String eName,
+                              @RequestParam String eDesc, @RequestParam String eDate,
+                              @RequestParam String eLocation, @RequestParam String eLongitude,
+                              @RequestParam String eLatitude) {
+        // Check if type is valid
+        String[] types = data.getTypes();
+        boolean isValid = false;
+        for (String compareType : types) {
+            if (!isValid) {
+                if (eType.equals(compareType)) {
+                    isValid = true;
+                }
+            }
+        }
+        if (!isValid) {
+            return "error";
+        }
+
+        // Parse date
+        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd");
+        Date startDate;
+        try {
+            startDate = parser.parse(eDate);
+        } catch (ParseException e) {
+            return "error";
+        }
+
+        // Create location string by checking if both field are used
+        String locationString;
+        if (eLocation.isEmpty()) {
+            if (!eLatitude.isEmpty() && !eLongitude.isEmpty()) {
+                locationString = eLatitude + " " + eLongitude;
+            } else {
+                return "error";
+            }
+        } else if (eLatitude.isEmpty() && eLongitude.isEmpty()) {
+            locationString = eLocation;
+        } else {
+            return "error";
+        }
+
+        // Create Event
+        Event eventToSave = new Event(eName, eType, startDate, locationString, eDesc);
+        try {
+            data.saveEvent(eventToSave);
+        } catch (NoSuchEvent e) {
+            return "error";
+        }
+
+        // Construct Site
+        Event[] events = data.getLastEvents(20);
+        model.addAttribute("types", types);
+        model.addAttribute("events", events);
+        return "home";
     }
 
-
+    /*
+    @ExceptionHandler(NoSuchEvent.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public @ResponseBody NoSuchEvent handleException(NoSuchEvent e) {
+        return new NoSuchEvent("Event doesn't exist!");
+    }
+    */
 }
