84cd98df35b027c75e48126ecca44d0542b01784
Lorenz Simon <simonlorenz@fim.uni-passau.de>
2020-06-18 20:23:27 +0200

    feat: Added DTOs

---
+++ src/main/java/Gruppe3/Webseite/dto/EventDto.java
@@ -0,0 +1,55 @@
+package Gruppe3.Webseite.dto;
+
+import java.util.Date;
+
+public class EventDto {
+
+    private final String name;
+    private final String type;
+    private final String location;
+    private final Date startDate;
+    private final String description;
+    private final int dislikes;
+    private final int likes;
+
+
+    public EventDto(final String name, final String type, final Date startDate,
+                    final String location, final String description,
+                    final int likes, final int dislikes) {
+        this.type = type;
+        this.location = location;
+        this.startDate = startDate;
+        this.name = name;
+        this.description = description;
+        this.dislikes = dislikes;
+        this.likes = likes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getDislikes() {
+        return dislikes;
+    }
+
+    public int getLikes() {
+        return likes;
+    }
+}
