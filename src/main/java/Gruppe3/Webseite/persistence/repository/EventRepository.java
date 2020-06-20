b7483dfe7044fc772b557e70bb8fac7c6e2bfb57
tionis.dev <git@tionis.dev>
2020-06-20 18:47:11 +0200

    feat: added database interaction for creating, voting, last events and top events

--- src/main/java/Gruppe3/Webseite/persistence/repository/EventRepository.java
+++ src/main/java/Gruppe3/Webseite/persistence/repository/EventRepository.java
@@ -2,9 +2,16 @@ package Gruppe3.Webseite.persistence.repository;
 
 import Gruppe3.Webseite.persistence.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, String> {
+    @Query(value = "SELECT * FROM event ORDER BY likes-dislikes DESC LIMIT :count", nativeQuery = true)
+    Event[] getTopEvents(@Param("count") Integer count);
 
+    @Query(value = "SELECT * FROM event ORDER BY creation_date DESC LIMIT :count", nativeQuery = true)
+    Event[] getLastEvents(@Param("count") Integer count);
 }
