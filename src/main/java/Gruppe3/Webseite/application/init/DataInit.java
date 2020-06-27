c6fce3357eb141e73701048ae74676c6019622f3
Lorenz Simon <simonlorenz@fim.uni-passau.de>
2020-06-27 19:19:27 +0200

    fix: Fixed accidental recreation of initialized events (only initialize database if init = true) and fixed path of stylesheet

--- src/main/java/Gruppe3/Webseite/application/init/DataInit.java
+++ src/main/java/Gruppe3/Webseite/application/init/DataInit.java
@@ -3,6 +3,7 @@ package Gruppe3.Webseite.application.init;
 import Gruppe3.Webseite.application.service.EventService;
 import Gruppe3.Webseite.persistence.entities.Event;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +25,12 @@ public class DataInit implements CommandLineRunner {
     private final Random rand;
 
     /**
+     * Value of init property
+     */
+    @Value("${init}")
+    private boolean init;
+
+    /**
      * Initialize the application.
      *
      * @param eventService the service the initializer is created with
@@ -41,29 +48,31 @@ public class DataInit implements CommandLineRunner {
      */
     @Override
     public void run(final String... args) {
-        String[] types = eventService.getTypes();
+        if (init) {
+            String[] types = eventService.getTypes();
 
-        // Add normal example events
-        for (int i = 0; i < types.length; i++) {
+            // Add normal example events
+            for (int i = 0; i < types.length; i++) {
+                Calendar cal = Calendar.getInstance();
+                cal.setTime(new Date());
+                cal.add(Calendar.DATE, i);
+                Date startDate = cal.getTime();
+                eventService.saveEvent(
+                        new Event("Event" + (i + 1), types[i],
+                                startDate, new Date(), "Munich",
+                                null, null, "Eine tolle Beschreibung",
+                                rand.nextInt(101), rand.nextInt(101)));
+            }
+
+            // Add coordinate based example event
             Calendar cal = Calendar.getInstance();
             cal.setTime(new Date());
-            cal.add(Calendar.DATE, i);
+            cal.add(Calendar.DATE, 4);
             Date startDate = cal.getTime();
-            eventService.saveEvent(
-                    new Event("Event" + (i + 1), types[i],
-                            startDate, new Date(), "Munich",
-                            null, null, "Eine tolle Beschreibung",
-                            rand.nextInt(101), rand.nextInt(101)));
+            eventService.saveEvent(new Event("Presidential Party",
+                    types[0], startDate, new Date(), null,
+                    38.897957, -77.036560,
+                    "The best party maybe ever!", 100, 10));
         }
-
-        // Add coordinate based example event
-        Calendar cal = Calendar.getInstance();
-        cal.setTime(new Date());
-        cal.add(Calendar.DATE, 4);
-        Date startDate = cal.getTime();
-        eventService.saveEvent(new Event("Presidential Party",
-                types[0], startDate, new Date(), null,
-                38.897957, -77.036560,
-                "The best party maybe ever!", 100, 10));
     }
 }
